5bf6aa46bb9dcd73495a8e10ef93a50fdf9a5f6b
Sony Hoang <sonyhoang53@gmail.com>
2025-02-12 19:47:09 +0000

    fix: rewording

--- element.sh
+++ element.sh
@@ -5,7 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=period_table -t --no-align -c"
 MAIN() {
   if [[ -z $! ]]
   then
-    echo "Please provide an element as an argument"
+    echo "Please provide an element as an argument."
   else
     ELEMENT $1
   fi
