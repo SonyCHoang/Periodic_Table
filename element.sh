2daafd94af65975583b1886b6deb9bd0128dfa6f
Sony Hoang <sonyhoang53@gmail.com>
2025-02-12 19:34:35 +0000

    feat: add main program

--- element.sh
+++ element.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=period_table -t --no-align -c"
+
+MAIN() {
+  if [[ -z $! ]]
+  then
+    echo "Please provide an element as an argument"
+  else
+    ELEMENT $1
+  fi
