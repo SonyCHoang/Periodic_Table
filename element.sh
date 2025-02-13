0da803a98791fd5a03d59fbae21e7f043fb601a8
Sony Hoang <sonyhoang53@gmail.com>
2025-02-13 19:15:15 +0000

    feat: add functionality

--- element.sh
+++ element.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=period_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 MAIN() {
-  if [[ -z $! ]]
+  if [[ -z $1 ]]
   then
     echo "Please provide an element as an argument."
   else
-    ELEMENT $1
+    ELEMENT_PROGRAM $1
   fi
 }
 
-MAIN
+ELEMENT_PROGRAM() {
+  ELEMENT=$1
+  if [[ $ELEMENT =~ ^[0-9]+$ ]]
+  then
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements where atomic_number=$ELEMENT")
+  else
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name=$ELEMENT OR symbol=$ELEMENT")
+  fi
+
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    MELTING_POINT_CELSIUS=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    BOILING_POINT_CELSIUS=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+    TYPE=$($PSQL "SELECT type FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER")
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+  fi
+}
+
+MAIN $1
